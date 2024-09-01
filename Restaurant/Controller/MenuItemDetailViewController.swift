09dba0f1d7af6ba83ce7e0695d6bd37b9e3ff739
MahmoudAlaa <mahmoudalaa.wr@gmail.com>
2024-09-01 17:23:47 +0300

    Add MVC Architecture & tabBar

---
+++ Restaurant/Controller/MenuItemDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Mahmoud Alaa on 01/09/2024.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
